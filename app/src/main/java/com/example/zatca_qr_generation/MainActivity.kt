46beff0d2478cb0ac5d27a6a22479f7f1709038a
Ibrahim <iabdelgawaad@gmail.com>
2021-11-23 01:33:57 +0200

    intial commit

---
+++ app/src/main/java/com/example/zatca_qr_generation/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.example.zatca_qr_generation
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeEncoder
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var sellerNameEditText = findViewById<TextView>(R.id.seller_name_edit)
+        var taxNumberEditText = findViewById<TextView>(R.id.tax_number_edit)
+        var dateTimeEditText = findViewById<TextView>(R.id.date_time_edit)
+        var totalAmountWithVatEditText = findViewById<TextView>(R.id.total_edit)
+        var vatAmountEditText = findViewById<TextView>(R.id.vat_amount_edit)
+        var convertButton = findViewById<TextView>(R.id.convert_button)
+
+//        val builder = ZatcaQRCodeGeneration.Builder()
+//        builder.sellerName("Marn Retail")
+//            .taxNumber("1234567891")
+//            .invoiceDate("2021-11-22T14:25:09Z")
+//            .totalAmount("100.00")
+//            .taxAmount("15.00")
+//        base64TextView.text = builder.getBase64()
+
+        convertButton?.setOnClickListener {
+            val builder = ZatcaQRCodeGeneration.Builder()
+            builder.sellerName(sellerNameEditText?.text?.toString())
+                .taxNumber(taxNumberEditText?.text?.toString())
+                .invoiceDate(dateTimeEditText?.text?.toString())
+                .totalAmount(totalAmountWithVatEditText?.text?.toString())
+                .taxAmount(vatAmountEditText?.text?.toString())
+            showBottomSheet(builder.getBase64())
+        }
+    }
+
+    private fun showBottomSheet(base64: String) {
+        val dialog = BottomSheetDialog(this)
+        val view = layoutInflater.inflate(R.layout.bottom_sheet_dialog, null)
+        val btnClose = view.findViewById<Button>(R.id.idBtnDismiss)
+        var base64TextView = view.findViewById<TextView>(R.id.base64text)
+        var qrCodeImageView = view.findViewById<ImageView>(R.id.qrcode_img)
+        var zatcaButton = view.findViewById<Button>(R.id.zatca_app_button)
+
+        base64TextView.text = base64
+        qrCodeImageView.setImageBitmap(generateQRCodeFromText(base64))
+
+        btnClose.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        zatcaButton?.setOnClickListener {
+            openEInvoiceAApp()
+        }
+
+        dialog.setCancelable(false)
+        dialog.setContentView(view)
+        dialog.show()
+    }
+
+    private fun generateQRCodeFromText(content: String? = ""): Bitmap? {
+        try {
+            val barcodeEncoder = BarcodeEncoder()
+            return barcodeEncoder.encodeBitmap(content, BarcodeFormat.QR_CODE, 300, 300)
+        } catch (e: Exception) {
+        }
+        return null
+    }
+
+    private fun openEInvoiceAApp() {
+        try {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://play.google.com/store/apps/details?id=com.posbankbh.einvoiceqrreader")
+                )
+            )
+        } catch (e: Exception) {
+        }
+    }
+}
---
+++ app/src/main/java/com/example/zatca_qr_generation/ZatcaQRCodeGeneration.kt
@@ -0,0 +1,93 @@
+package com.example.zatca_qr_generation
+
+import android.util.Base64
+
+class ZatcaQRCodeGeneration {
+    class Builder {
+        val SELLER_NAME_TAG = "1"
+        val TAX_NUMBER_TAG = "2"
+        val INVOICE_DATE_TAG = "3"
+        val TOTAL_AMOUNT_TAG = "4"
+        val TAX_AMOUNT_TAG = "5"
+
+        var sellerName: String = ""
+        var taxNumber: String = ""
+        var invoiceDate: String = ""
+        var totalAmount: String = ""
+        var taxAmount: String = ""
+
+        fun sellerName(value: String?) = apply {
+            if (value != null) {
+                this.sellerName = value
+            }
+        }
+
+        fun taxNumber(value: String?) = apply {
+            if (value != null) {
+                this.taxNumber = value
+            }
+        }
+
+        fun invoiceDate(value: String?) = apply {
+            if (value != null) {
+                this.invoiceDate = value
+            }
+        }
+
+        fun totalAmount(value: String?) = apply {
+            if (value != null) {
+                this.totalAmount = value
+            }
+        }
+
+        fun taxAmount(value: String?) = apply {
+            if (value != null) {
+                this.taxAmount = value
+            }
+        }
+
+        private fun convertTagsAndLengthToHexValues(
+            tag: String,
+            length: String,
+            value: String
+        ): ByteArray {
+            return byteArrayOf(tag.toByte(), length.toByte()).plus(value.toByteArray())
+        }
+
+        fun getBase64(): String {
+
+            val tlv1 = convertTagsAndLengthToHexValues(
+                SELLER_NAME_TAG,
+                sellerName.length.toString(),
+                sellerName
+            )
+
+            val tlv2 = convertTagsAndLengthToHexValues(
+                TAX_NUMBER_TAG,
+                taxNumber.length.toString(),
+                taxNumber
+            )
+
+            val tlv3 = convertTagsAndLengthToHexValues(
+                INVOICE_DATE_TAG,
+                invoiceDate.length.toString(),
+                invoiceDate
+            )
+
+            val tlv4 = convertTagsAndLengthToHexValues(
+                TOTAL_AMOUNT_TAG,
+                totalAmount.length.toString(),
+                totalAmount
+            )
+
+            val tlv5 = convertTagsAndLengthToHexValues(
+                TAX_AMOUNT_TAG,
+                taxAmount.length.toString(),
+                taxAmount
+            )
+
+            var tlvs = tlv1 + tlv2 + tlv3 + tlv4 + tlv5
+            return Base64.encodeToString(tlvs, Base64.DEFAULT)
+        }
+    }
+}
